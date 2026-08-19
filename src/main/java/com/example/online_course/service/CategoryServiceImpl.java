5fe3de13b43214aa014d938b6f8087f3dc301a11
Savry <savryrym14@gmail.com>
2026-08-19 16:53:12 +0700

    update security

--- src/main/java/com/example/online_course/service/CategoryServiceImpl.java
+++ src/main/java/com/example/online_course/service/CategoryServiceImpl.java
@@ -12,15 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+
 
 @Service
 @RequiredArgsConstructor
